9edac25d1723e9078f5c56fa25c626a026b2b3aa
Dineth <dinethnethsara24@gmail.com>
2026-01-27 23:17:27 +0530

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello</h1>
+      <h2>my name is dineth</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+
+    return(
+        <div>
+            <h1>Dineth Nethsara</h1>
+            <p>lorem ipsum dolor sitw amet</p>
+        </div>
+    )
+}
